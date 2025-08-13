a16816d91317cc8809b269f4204974f9401f7501
NHU TRUNG HAI <nhuthifc@gmail.com>
2025-08-14 06:18:43 +0700

    update page

--- controllers/admins/products.controller.js
+++ controllers/admins/products.controller.js
@@ -2,14 +2,23 @@ const databaseProducts = require("../../model/product.model");
 module.exports.products = async (req, res) => {
   let keySearch = "";
   const query = { deleted: false };
-  if (req.query.status) query.status = req.query.status;
-  if (req.query.search) {
-    keySearch = req.query.search;
+  let {status:queryStatus,search:querySearch,page:queryPage} = req.query
+  if (queryStatus) query.status = queryStatus;
+  if (querySearch) {
+    keySearch = querySearch;
     query.title = new RegExp(`^${keySearch}`, "i");
   }
-  const products = await databaseProducts.find(query);
+  let products = await databaseProducts.find(query);
   const lengthPage = Math.ceil(products.length / 4);
   const arr = Array.from({ length: lengthPage }, (_, i) => i + 1);
+  
+  if (!queryPage || isNaN(queryPage)) {
+    queryPage = 1;
+  } 
+
+  const startPage = (queryPage - 1)* 4
+  const endPage = queryPage * 4
+  products = products.slice(startPage,endPage)
   res.render("./admins/pages/products/index.pug", {
     title: "Quản lý sản phẩm",
     products: products,
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,10 +1,13 @@
 const buttonStatus = document.querySelectorAll("button[data-set]");
 const url = new URL(location.href);
-function handleUrl(key, value) {
+function handleUrl(key, value,flag = true) {
   if (value) {
     url.searchParams.set(key, value);
   } else {
     url.searchParams.delete(key);
+  }
+  if(flag){
+    url.searchParams.set('page','1')
   }
   location.href = url.href;
 }
@@ -22,20 +25,21 @@ formSearch.addEventListener("submit", function (e) {
   const valueInput = this.elements.search.value;
   handleUrl("search", valueInput);
 });
-
 // handle page
 const querySearch = new URLSearchParams(location.search);
-const page = querySearch.get("page");
-// if(page){
-
-// }
-console.log(document.querySelector(`.page-item[data-set]`).innerText);
+let page = querySearch.get("page");
+if(!page) {
+  page = 1
+  querySearch.set('page','1')
+  history.pushState(null,null,`?${querySearch}${location.hash}`)
+}
+const pageActive = document.querySelector(`.page-item[data-set="${page}"]`)
+if(pageActive) pageActive.classList.add('active')
 
 const pagination = document.querySelectorAll(".page-item");
 pagination.forEach((element) => {
   element.onclick = () => {
     const page = element.dataset.set;
-    console.log(page);
-    handleUrl("page", page);
+    handleUrl("page", page,false);
   };
 });
