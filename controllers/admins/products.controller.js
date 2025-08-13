14e0c7c5ad37d60893781e9954801b6763e33724
nhutrunghai <nhthifc@gmail.com>
2025-08-13 12:48:03 +0700

    update page product

--- controllers/admins/products.controller.js
+++ controllers/admins/products.controller.js
@@ -1,16 +1,19 @@
-const databaseProducts = require('../../model/product.model')
-module.exports.products = async (req,res) => {
-    let keySearch = "";
-    const query = {deleted:false}
-    if(req.query.status) query.status = req.query.status
-    if(req.query.search) {
-        keySearch = req.query.search
-        query.title = new RegExp(`^${keySearch}`,'i')
-    }
-    const products = await databaseProducts.find(query)
-    const lengthPage = Math.ceil(products.length / 4) 
-    const arr = Array.from({ length: lengthPage },(_,i) => i + 1);
-    console.log({arr});
-    
-    res.render('./admins/pages/products/index.pug', {title : 'Quản lý sản phẩm',products:products,keySearch:keySearch})
-}
+const databaseProducts = require("../../model/product.model");
+module.exports.products = async (req, res) => {
+  let keySearch = "";
+  const query = { deleted: false };
+  if (req.query.status) query.status = req.query.status;
+  if (req.query.search) {
+    keySearch = req.query.search;
+    query.title = new RegExp(`^${keySearch}`, "i");
+  }
+  const products = await databaseProducts.find(query);
+  const lengthPage = Math.ceil(products.length / 4);
+  const arr = Array.from({ length: lengthPage }, (_, i) => i + 1);
+  res.render("./admins/pages/products/index.pug", {
+    title: "Quản lý sản phẩm",
+    products: products,
+    keySearch: keySearch,
+    arrPage: arr,
+  });
+};
--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -1,25 +1,41 @@
-const buttonStatus = document.querySelectorAll('button[data-set]')
-const url = new URL(location.href)
-function handleUrl(key,value){
-    if(value){
-        url.searchParams.set(key,value)
-    }else{
-        url.searchParams.delete(key)
-    }
-    location.href = url.href
+const buttonStatus = document.querySelectorAll("button[data-set]");
+const url = new URL(location.href);
+function handleUrl(key, value) {
+  if (value) {
+    url.searchParams.set(key, value);
+  } else {
+    url.searchParams.delete(key);
+  }
+  location.href = url.href;
 }
-buttonStatus.forEach(element => {
-    element.onclick = ()=> {
-        const status = element.dataset.set
-        handleUrl('status',status)   
-    }
-
+buttonStatus.forEach((element) => {
+  element.onclick = () => {
+    const status = element.dataset.set;
+    handleUrl("status", status);
+  };
 });
 
 // handle search
-const formSearch = document.querySelector('#search-product')
-formSearch.addEventListener('submit',function(e){
-    e.preventDefault()
-    const valueInput = this.elements.search.value
-    handleUrl('search',valueInput)
-})
+const formSearch = document.querySelector("#search-product");
+formSearch.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const valueInput = this.elements.search.value;
+  handleUrl("search", valueInput);
+});
+
+// handle page
+const querySearch = new URLSearchParams(location.search);
+const page = querySearch.get("page");
+// if(page){
+
+// }
+console.log(document.querySelector(`.page-item[data-set]`).innerText);
+
+const pagination = document.querySelectorAll(".page-item");
+pagination.forEach((element) => {
+  element.onclick = () => {
+    const page = element.dataset.set;
+    console.log(page);
+    handleUrl("page", page);
+  };
+});
